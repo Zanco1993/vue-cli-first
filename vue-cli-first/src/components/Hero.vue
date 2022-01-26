a6e387566d7d3f4cb63a34344225518e21c42297
Zanco1993 <zanconatofabio@gmail.com>
2022-01-26 15:56:19 +0100

    esercizio + BONUS completato

---
+++ vue-cli-first/src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="jumbotron">
+    <h2>AVENGERS</h2>
+    <img src="../assets/avengers.jpg">
+</div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .jumbotron {
+        width: 800px;
+        margin: 0 auto;
+    }
+    .jumbotron img {
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
